f2197d44cd0010bbc31c0a64c76df0ac75a79e06
Matthijs <matthijs.vannistelrooij@gmail.com>
2023-12-16 15:03:39 +0100

    update text description

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: "Evently",
-  description: "Evently is a platform for event management",
+  title: "Sardientjes",
+  description: "Sardientjes is a platform for sardientjes",
   icons: {
     icon: "/assets/images/logo.svg",
   },
